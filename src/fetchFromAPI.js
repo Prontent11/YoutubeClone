910bdecf08d07527a4ccbd5870756aef29f5327c
= <yashyadao2001@gmail.com>
2023-02-03 15:22:59 +0530

    fixed channel card ui bug

--- src/fetchFromAPI.js
+++ src/fetchFromAPI.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 const BASE_URL = "https://youtube-v31.p.rapidapi.com";
-
 const options = {
   url: BASE_URL,
   params: {
@@ -15,6 +14,6 @@ const options = {
 
 export const fetchFromAPI = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-  
+
   return data;
 };
